715fa2b6642b8abb627afdeb711d34a0397f1563
mahmoudnaga <mahmoudaboelnaga@yahoo.com>
2020-10-18 21:04:33 +0200

    add redux to filter component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,9 @@ import React, { Component } from 'react';
 import Cart from './components/Cart';
 import Filter from './components/Filter';
 import Products from './components/Products';
-import data from "./data.json";
 
 class App extends Component {
   state = {
-    products: data.products,
-    size: "",
-    sort: "",
     cartItems: localStorage.getItem("cartItems") ? JSON.parse(localStorage.getItem("cartItems")) : []
   };
 
@@ -41,64 +37,28 @@ class App extends Component {
   createOrder = (order) => {
     alert(`Need to save order for ${order.name}`);
   };
-  filterProducts = (event) => {
-    if (event.target.value === "") {
-      this.setState({
-        size: event.target.value,
-        products: data.products
-      });
-    } else {
-      this.setState({
-        size: event.target.value,
-        products: data.products.filter(product => product.availableSizes.indexOf(event.target.value) >= 0)
-      });
-    }
-  };
-
-  sortProducts = (event) => {
-    const sort = event.target.value;
-    this.setState({
-      sort: sort,
-      products: this.state.products.slice().sort((a, b) =>
-        sort === "lowest"
-          ? a.price > b.price
-            ? 1
-            : -1
-          : sort === "highest"
-            ? a.price < b.price
-              ? 1
-              : -1
-            : a._id < b._id
-              ? 1
-              : -1
-      )
-    });
-  };
 
   render() {
     return (
-        <div className="grid-container" >
-          <header>
-            <a href="/">React Shopping Cart</a>
-          </header>
-          <main>
-            <div className="content">
-              <div className="main">
-                <Filter count={this.state.products.length} size={this.state.size} sort={this.state.sort}
-                  filterProducts={this.filterProducts}
-                  sortProducts={this.sortProducts}
-                />
-                <Products products={this.state.products} addToCart={this.addToCart} />
-              </div>
-              <div className="sidebar">
-                <Cart cartItems={this.state.cartItems} removeFromCart={this.removeFromCart} createOrder={this.createOrder} />
-              </div>
+      <div className="grid-container" >
+        <header>
+          <a href="/">React Shopping Cart</a>
+        </header>
+        <main>
+          <div className="content">
+            <div className="main">
+              <Filter />
+              <Products  addToCart={this.addToCart} />
             </div>
-          </main>
-          <footer>
-            All rights reserved &reg; copyright &copy; 2020.
+            <div className="sidebar">
+              <Cart cartItems={this.state.cartItems} removeFromCart={this.removeFromCart} createOrder={this.createOrder} />
+            </div>
+          </div>
+        </main>
+        <footer>
+          All rights reserved &reg; copyright &copy; 2020.
           </footer>
-        </div>
+      </div>
     );
   }
 }
